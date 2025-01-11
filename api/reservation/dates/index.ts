aafd902b98e73b8902d3efaa33a98ba15fa8882b
Emiliano Carmona <elcarmon@gmail.com>
2025-01-11 20:58:06 -0300

    Remove type:module to make Vercel api routes work!

--- api/reservation/dates/index.ts
+++ api/reservation/dates/index.ts
@@ -1,6 +1,10 @@
 import { getDateHTML } from '../../../src/common/dateHTML';
 import type { Mode } from '../../../src/common/dateHTML';
 export async function GET(req: Request) {
-  const [, mode] = req.url.match(/\?mode=([a-z]+)$/);
-  return new Response(getDateHTML(mode as Mode));
+  const res = req.url.match(/\?mode=([a-z]+)$/);
+  if (res) {
+    const [, mode] = res;
+    return new Response(getDateHTML(mode as Mode));
+  }
+  return new Response('');
 }
